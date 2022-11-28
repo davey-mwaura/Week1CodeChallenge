73683c1aea77fc486bda313671eeb2acd079379d
davey-mwaura <davidmburu50@gmail.com>
2022-11-28 20:42:49 +0300

    Completed two assignments

--- ChallengeOne.js
+++ ChallengeOne.js
@@ -2,22 +2,27 @@ function CheckGrade(marks){
 
 console.log("Your Grade is :")
 
+// Grade A
 if (marks > 79){
     console.log("A")
 }
 
+// Grade B
 else if (marks >= 60){
     console.log("B")
 }
 
+// Grade C
 else if (marks >= 49){
     console.log("C")
 }
 
+// Grade D
 else if (marks >= 40){
     console.log("D")
 }
 
+// Grade E
 else if(marks < 40){
     console.log("E")
 }
